695b17f3d9ba7648a8972ca577ad5e75063e4035
Bobrik-yryr <92583388+Bobrik-yryr@users.noreply.github.com>
2021-10-19 22:05:25 +0300

    First commit

---
+++ app/src/main/java/com/barabanovk/loftschooll/MainActivity.java
@@ -0,0 +1,26 @@
+package com.barabanovk.loftschooll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
